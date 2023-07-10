cf1a30126610d961beb56ff659e089de947cfd59
ERR68 <138792826+ERR68@users.noreply.github.com>
2023-07-10 09:47:03 +0300

    Add files via upload

--- zthon/__main__.py
+++ zthon/__main__.py
@@ -18,7 +18,7 @@ print(f"المرخصة بموجب شروط  {zthon.__license__}")
 cmdhr = Config.COMMAND_HAND_LER
 
 try:
-    LOGS.info("✧ بـدء تنزيـل اللوري ✧")
+    LOGS.info("✧ بـدء تنزيـل اللـوري ✧")
     zedub.loop.run_until_complete(setup_bot())
     LOGS.info("✧ بـدء تشغيـل البـوت ✧")
 except Exception as e:
@@ -63,7 +63,7 @@ async def startup_process():
 
     async def start_bot():
       try:
-          List = ["Tepthon","P17_12","Tepthone1","Tws_Tepthon","Tepthon_Help","super_tepthon","tepthonklaesh","x_wwl","K_K_Q_L","VisaTepthon","zszxzz"]
+          List = ["SOURCE_ELLORY","ELLORY_SUPPORT","SUPER_ELLORY"]
           from telethon.tl.functions.channels import JoinChannelRequest
           for id in List :
               Join = await zedub(JoinChannelRequest(channel=id))
@@ -77,7 +77,7 @@ async def startup_process():
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
-    print(f"⌔┊تـم تنصيـب اللـوري . . بنجـاح ✓ \n⌔┊لـ إظهـار الاوامر أرسـل (.الاوامر)")
+    print(f"⌔┊تـم تنصيـب اللـوري . . بنجـاح ✓ \n⌔┊لـ إظهـار الاوامـر ارسـل (.الاوامر)")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
